4abae782af39c34de5b5489abf6a158b02a72bcc
Alexey Kulagin <proctoleha@gmail.com>
2019-02-09 08:02:24 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -8,8 +8,9 @@ use Dotenv\Dotenv;
 
 (Dotenv::create($path, null, new DotenvFactory([new Adapter()])))->load();
 
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+defined('YII_DEBUG') or define('YII_DEBUG', $_ENV['YII_DEBUG']);
+defined('YII_ENV') or define('YII_ENV', $_ENV['YII_ENV']);
 
-$config = require __DIR__ . '/../config/web.php';
-
+require $path . '/vendor/yiisoft/yii2/Yii.php';
+$config = require $path . '/config/web.php';
 (new yii\web\Application($config))->run();
